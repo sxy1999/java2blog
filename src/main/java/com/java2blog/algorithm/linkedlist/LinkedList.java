f682314d412947da5ff22214e227f29d403dbea5
Tom Sun <tsun@donbest.com>
2017-07-05 14:20:20 -0700

    added middle of linkedlist.

--- src/main/java/com/java2blog/algorithm/linkedlist/LinkedList.java
+++ src/main/java/com/java2blog/algorithm/linkedlist/LinkedList.java
@@ -133,4 +133,21 @@ public class LinkedList {
 			return 1 + lengthOfLinkedListRec(temp.next);
 		}
 	}
+
+	// This function will find middle element in linkedlist
+	public Node findMiddleNode(Node head) {
+		Node slowPointer, fastPointer;
+		slowPointer = fastPointer = head;
+
+		while (fastPointer != null) {
+			fastPointer = fastPointer.next;
+			if (fastPointer != null && fastPointer.next != null) {
+				slowPointer = slowPointer.next;
+				fastPointer = fastPointer.next;
+			}
+		}
+
+		return slowPointer;
+
+	}
 }
